37b8a17725f486c9d43a53881925f412a2522f83
Noah Schumacher <noahschumacher14@gmail.com>
2019-03-15 09:37:40 -0700

    Putting together website for fraud v1

--- website/models/predict.py
+++ website/models/predict.py
@@ -1,11 +1,12 @@
 import pickle
-import clean
+import models.clean
 import pandas as pd
 
 from pymongo import MongoClient
 
 ## Convert dictionary into pandas frame
 def make_pandas(entry):
+	print(entry)
 	df = pd.DataFrame(entry)
 
 	## Clean the data
@@ -22,7 +23,7 @@ def predict(model, cleaned):
 
 
 
-if __name__ == '__main__':
+def get_prediction():
 
 	## Read in the pickle model
 	model = pickle.load(open('models/rf_model.p', 'rb'))
@@ -30,15 +31,16 @@ if __name__ == '__main__':
 	## Connect to the Mongo DB
 	client = MongoClient('localhost', 27017)
 	db = client['fraud']		## Mongo database name
-	table = db['events_new2']	## Mongo table name
+	table = db['events']		## Mongo table name
 
 	## Read in the first X new entries
-	r = table_new.find().sort([('_id', -1)]).limit(1)
+	r = table.find().sort([('_id', -1)]).limit(2)
 
+	print(type(r))
 	## Transforming data
 	cleaned = make_pandas(r[0])
 
 	## Predict on the new data
-	prediction = predict(model, cleaned)
+	return predict(model, cleaned)
 	
 
--- website/server.py
+++ website/server.py
@@ -5,6 +5,8 @@ import pickle
 import numpy as np
 import pandas as pd
 
+import models.predict as predict
+
 ## Create the app object that will route our calls
 app = Flask(__name__)
 
@@ -14,60 +16,28 @@ app = Flask(__name__)
 def home():
 	return render_template('home.html')
 
-## Rendering the mpg prediction page html
-@app.route('/mpg', methods = ['GET'])
-def mpg():
-	return render_template('mpg.html')
 
 ## Calculating and posting the linear regression model prediction
-modelLR = pickle.load(open('linreg.p', 'rb'))
-@app.route('/inferenceLR', methods = ['POST'])
-def inferenceLR():
-	req = request.get_json()
-	print(req)
+modelLR = pickle.load(open('models/rf_model.p', 'rb'))
+@app.route('/prediction', methods = ['POST'])
+def prediction():
+	print("GET HERE")
+	#req = request.get_json()
+	#print(req)
+	print("HEREEE")
 
-	## Getting params from request
-	c,h,w = [req['cylinders'], req['horsepower'], req['weight']]
-	prediction = list(modelLR.predict([[c,h,w]])) ## Getting prediction
+	pred = predict.get_prediction()
 
 	## Returning json formatted output (.js file grabs 'prediction')
-	return jsonify({'c':c, 'h':h, 'w':w, 'prediction':np.round(prediction[0],3)})
+	return jsonify({'prediction':np.round(prediction,3)})
 
 
-modelRF = pickle.load(open('randomf.p', 'rb'))
-@app.route('/inferenceRF', methods = ['POST'])
-def inferenceRF():
-	req = request.get_json()
-	print(req)
 
-	params = [req['cylinders'], 
-			  req['displacement'],
-		 	  req['horsepower'],
-		 	  req['weight'],
-		 	  req['acceleration'],
-		 	  req['model'],
-		 	  req['origin']]
-	print(params)
-
-	## Checking if all the parameters are entered
-	if None in params:
-		return jsonify({'prediction':'Need to enter all data'})
-
-	## If all entered getting and posting prediciton
-	else:
-		prediction = list(modelRF.predict([params]))
-		
-		return jsonify({'c':params[0],
-						'h':params[1],
-						'w':params[2],
-						'prediction':prediction[0]})
-
-
-@app.route('/plot', methods = ['GET'])
-def plot():
-	df = pd.read_csv('cars.csv')
-	data = list(zip(df.mpg, df.weight))
-	return jsonify(data)
+# @app.route('/plot', methods = ['GET'])
+# def plot():
+# 	df = pd.read_csv('cars.csv')
+# 	data = list(zip(df.mpg, df.weight))
+# 	return jsonify(data)
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=3333, debug=True)
